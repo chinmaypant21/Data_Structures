5c7c4086b8839ead6b23539da17704b5f47cc198
chinmaypant21 <pant.chinu.2@gmail.com>
2021-08-21 14:45:25 +0530

    Array implementation for python

---
+++ Arrays/3_Array_implementation.py
@@ -0,0 +1,41 @@
+class Array:
+
+    def __init__(self):
+        self.length=0
+        self.data=[]
+
+    def at(self,index):
+        return self.data[index]
+
+    def push(self,element):
+        self.data.append(element)
+        self.length+=1
+
+    def pop(self):
+        del(self.data[-1])
+        self.length-=1
+
+    def shift(self,index):
+        for i in range(index,self.length-1):
+            self.data[i] = self.data[i+1]
+        del(self.data[-1])
+
+    def delete(self,index):
+        self.shift(index)
+        self.length-=1
+
+    def __str__(self): 
+        return " ".join([str(elem) for elem in self.data])
+
+myArray = Array()
+myArray.push(1)
+myArray.push(2)
+myArray.push(3)
+myArray.push(4)
+print(myArray) #after push
+
+myArray.pop()
+print(myArray) #after pop
+
+myArray.delete(1) #delete from index 2
+print(myArray)
