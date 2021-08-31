15ea9a37422b3ac5743c58e5bc1dab7b3bccb847
chinmaypant21 <pant.chinu.2@gmail.com>
2021-08-31 12:18:27 +0530

    Introduction Documentation and  Intro in Py

--- Linked List/1_Introduction.js
+++ Linked List/1_Introduction.js
@@ -2,16 +2,16 @@ class Node
 {
     constructor(value=0)
     {   
-        this.data = value
-        this.next = null
+        this.data = value //This will store the value of the data
+        this.next = null //This is a reference will point to the next node
     }  
 }
 
-class List
+class List  //This is the LinkedList
 {
     constructor(value=0)
     {
-        newNode = new Node(value)
+        newNode = new Node(value) //This creates the instance of Node object
         this.head = newNode
         this.tail = newNode
         this.length=1
