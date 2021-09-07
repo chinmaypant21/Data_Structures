fceb824f683efe4614bcf527b1b7b5832ef1f269
chinmaypant21 <pant.chinu.2@gmail.com>
2021-09-07 17:44:52 +0530

    Documentation CPP

--- Linked List/6_Merge.cpp
+++ Linked List/6_Merge.cpp
@@ -50,21 +50,35 @@ class List
     //Merging two Sorted LinkedList
     void merge(List l2)
     {
+        /**
+         * Initially Pointer first will point to the head of current LinkedList
+         * Initially Pointer second will point to the head of second LinkedList.
+         */
         Node *first = (*this).head;
         Node *second = l2.head;
 
-        if(first->data < second->data)  first = first->next;
+        /**
+         * Because we are merging sorted Lists, so our current head should point
+         * to the element which is smaller in the of the two elements of both Lists.
+         */
+        if(first->data < second->data)  first = first->next; //initially head is already same as first
         else
         {
             this->head = second;
             second = second->next;
         }
-
+        //This last pointer will point to the element before 'first' or 'second' pointer and 
+        //it will help to make nodes point in such a way that the resultant list is all sorted
         Node *last = this->head;
         last->next = NULL;
         
         while(first != NULL and second != NULL)
         {
+            /*If value at the Node to which first pointer is pointing is 
+            greater that the value for second, then the node after the last node should be 
+            the second one because its value is smaller than the first one.
+            And after this, the 'last' pointer will point to the second Node and
+            'second' pointer will point to the next node of current node.*/
             if(first->data>second->data)
             {
                 last->next=second;
@@ -79,8 +93,12 @@ class List
                 first = first->next;
             }
         }
-        
+
         last->next = NULL;
+        /*Now, if any of the pointers pointing first or second list are not null,
+        It means that some values are still remaining in that List.
+        So we point out next pointer of out last node to that Node(because definitely, the value(s))
+        that are remaining in that node are greater that the value at last node and all Lists are sorted.*/
         if(first!=NULL) last->next=first;
         else if(second!=NULL) last->next=second;
     }
