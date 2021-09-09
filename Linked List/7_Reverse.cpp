f43490e09d2c9a39b991f0d9346a6d8f75b7be35
chinmaypant21 <pant.chinu.2@gmail.com>
2021-09-09 13:41:57 +0530

    Reversing using sliding pointers CPP

---
+++ Linked List/7_Reverse.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    Node(int value=0)
+    {
+        this->data=value;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *tail;
+    Node *head;
+    int length {};
+
+    public:
+    List(int value=0)
+    {
+        Node *newNode = new Node(value);
+        this->head = newNode;
+        this->tail = newNode;
+        this->length++;
+    }
+
+    void display()
+    {
+        Node *current = this->head;
+        while(current != NULL)
+        {
+            cout<<current->data<<" "; 
+            current = current->next;
+        }
+    }
+
+    void push(int value)
+    {   
+        Node *newNode = new Node(value);
+        this->tail->next = newNode;
+        this->tail = newNode;
+        newNode->next = NULL;
+        this->length++;
+    }
+
+    // N elements copied from LL to array, then N elements copied from array to LL,
+    // thus Time Complexity = O(2n) = O(n)
+    void reverse_data()
+    {
+        int data_array[this->length] {0};
+        Node *current=this->head;
+        int i=0;
+        while(current!=NULL)
+        {
+            data_array[i++]=current->data;
+            current=current->next;
+        }
+        current=this->head;
+        while(current!=NULL)
+        {
+            current->data=data_array[--i];
+            current=current->next;
+        }
+    }
+
+    void reverse_nodes()
+    {
+        /* Reversing using slider Pointers method.*/
+        /**
+         * @brief In this method, we create thiree pointers, one pointing to the
+         * current element we are working on, one on the previous element, and one on the next element
+         * At starting prev and curr pointer are pointing to NULL, and after pointer to head
+         * then we slide pointers to next location and while doing it, we point the 'next' of current to 
+         * previous. We do this untill 'after' pointer gets to NULL.
+         */
+        Node *previous = NULL;
+        Node *current = NULL;
+        Node *after = head;
+        while(after!=NULL)
+        {
+            previous=current;
+            current=after;
+            after=after->next;
+            current->next=previous;
+        }
+        head=current;
+    }
+};
+
+int main()
+{
+    List l(2);
+    l.push(3);
+    l.push(4);
+    l.push(6);
+    l.reverse_nodes();
+
+    l.display();
+    return 0;
+}
