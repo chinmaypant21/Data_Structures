06fc3070d21cdaa1e5e92d99dd61f39a38ca57e5
chinmaypant21 <pant.chinu.2@gmail.com>
2021-09-16 13:16:40 +0530

    DLL Remove

--- Linked List/10_DLL_remove.cpp
+++ Linked List/10_DLL_remove.cpp
@@ -12,7 +12,7 @@ class Node
     In a doubly linked list, have two pointers previous and next which keeps track of the node
     previous to the current node and node after the current node in the List, thus it is DOUBLY LINKED.*/
 
-     Node(int value=0)
+    Node(int value=0)
     {
         this->data=value;
         next = NULL;
@@ -37,7 +37,7 @@ class DLList
         this->length++;
     }
 
-    void push(int value) //O(1)
+    void push(int value)
     {
         Node *newNode = new Node(value);
         this->tail->next = newNode;
@@ -47,13 +47,45 @@ class DLList
         this->length++;
     }
 
-    void pop() //O(1)
+    void remove(int index)
     {
-        this->tail  = this->tail->previous;
-        this->tail->next = NULL;
+        if(index > this->length-1 || index<0)
+        {
+            cout<<("\n[-]Invalid index for remove method\n");
+            return;
+        }
+
         this->length--;
-        /*In a DLL, pop has became much more easy than in SLL because of the 
-        availability of "previous node pointer"*/
+        Node *current = this->head;
+        if(index==0)
+        {
+            this->head = this->head->next;
+            this->head->previous=NULL;
+            delete current;
+            return;
+        }
+        /*
+        Here we are going to the node which we have to remove and then assigning the
+        "next" pointer of previous node to current's next and "previous" of  next node
+        to current's previous node.
+
+        We could also have done this by going to the element previous to the index element
+        and set its next to next's next (means we are just kicking out the given index element)
+        and current->next->previous to current.
+        */
+        for(int i=0;i<index;i++)
+        {
+            current = current->next;
+        }
+
+        current->previous->next = current->next;
+        if (current->next!=NULL)  //if curent is not the last node of DLL
+        {
+            current->next->previous=current->previous;
+        }
+
+        else this->tail = current->previous;
+        delete current;
     }
 
     void display()
@@ -77,7 +109,6 @@ int main()
     dll.push(2);
     dll.push(9);
     dll.display();
-    dll.pop();
+    dll.remove(0);
     dll.display();
-
 }
